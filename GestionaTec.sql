280a8834ad3d5d39ffdc758df3f1b322c237c5d4
joel2063 <joel.tene@unl.edu.ec>
2026-05-31 17:20:05 -0500

    Update GestionaTec.sql
    Mejorando la descripcion de atributos

--- GestionaTec.sql
+++ GestionaTec.sql
@@ -2,7 +2,7 @@ CREATE DATABASE GestionaTec;
 USE GestionaTec;
 
 CREATE TABLE USUARIO (
-    id_usuario INT AUTO_INCREMENT,
+    id_usuario VARCHAR (50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     contraseña VARCHAR(20) NOT NULL,
     rol ENUM('ADMIN', 'EMPLEADO') NOT NULL,
@@ -10,7 +10,7 @@ CREATE TABLE USUARIO (
     PRIMARY KEY (id_usuario)
 );
 CREATE TABLE HORARIO (
-    id_horario INT AUTO_INCREMENT,
+    id_horario VARCHAR(50) NOT NULL,
     entrada_matutina TIME NOT NULL,
     salida_matutina TIME NOT NULL,
     entrada_vespertina TIME NOT NULL,
@@ -19,14 +19,14 @@ CREATE TABLE HORARIO (
     PRIMARY KEY (id_horario)
 );
 CREATE TABLE CARGO (
-    id_cargo INT AUTO_INCREMENT,
+    id_cargo VARCHAR(50) NOT NULL,
     nombre_cargo VARCHAR(100) NOT NULL,
     sueldo DOUBLE NOT NULL,
 
     PRIMARY KEY (id_cargo)
 );
 CREATE TABLE EMPLEADO (
-    id_empleado INT AUTO_INCREMENT,
+    id_empleado VARCHAR(50) NOT NULL,
     nombre VARCHAR(100) NOT NULL,
     cedula VARCHAR(10) NOT NULL UNIQUE,
     direccion VARCHAR(10)NOT NULL,
@@ -42,7 +42,7 @@ CREATE TABLE EMPLEADO (
     foreign key (id_cargo) references CARGO(id_cargo)
 );
 CREATE TABLE ASISTENCIA (
-    id_asistencia INT AUTO_INCREMENT PRIMARY KEY,
+    id_asistencia VARCHAR (50) NOT NULL,
     fecha DATE NOT NULL,
     entrada_matutina TIME,
     salida_matutina TIME,
@@ -57,7 +57,7 @@ CREATE TABLE ASISTENCIA (
 );
 
 CREATE TABLE DESCUENTO (
-    id_descuento INT AUTO_INCREMENT,
+    id_descuento VARCHAR (50) NOT NULL,
     motivo VARCHAR(100) NOT NULL,
     tipo ENUM('HORAS', 'DIA') NOT NULL,
     monto DOUBLE NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE DESCUENTO (
 );
 
 CREATE TABLE JUSTIFICACION (
-    id_justificacion INT AUTO_INCREMENT,
+    id_justificacion VARCHAR (50) NOT NULL,
     tipo_justificacion ENUM('ATRASO', 'FALTA') NOT NULL,
     motivo VARCHAR(200) NOT NULL,
     horas_justificadas INT NOT NULL,
@@ -82,7 +82,7 @@ CREATE TABLE JUSTIFICACION (
     foreign key (id_asistencia) references ASISTENCIA(id_asistencia)
 );
 CREATE TABLE HORASEXTRA (
-    id_horaextra INT AUTO_INCREMENT,
+    id_horaextra INT VARCHAR(50) NOT NULL,
     fecha DATE NOT NULL,
     hora_inicio_extra TIME NOT NULL,
     hora_fin_extra TIME NOT NULL,
@@ -93,7 +93,7 @@ CREATE TABLE HORASEXTRA (
     foreign key (id_asistencia) references ASISTENCIA(id_asistencia)
 );
 CREATE TABLE REPORTE (
-    id_reporte INT AUTO_INCREMENT,
+    id_reporte VARCHAR(50) NOT NULL,
     total_horas INT NOT NULL DEFAULT 0,
     total_horas_extra INT NOT NULL DEFAULT 0,
     total_atrasos INT NOT NULL DEFAULT 0,
